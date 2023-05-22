296c8b737a23e05054326a4ffd8637bd351d905b
Guillaume <gthvn1@gmail.com>
2023-05-22 10:51:46 +0200

    doubly linked list: redo from scratch

--- src/doubly_linked_list.rs
+++ src/doubly_linked_list.rs
@@ -1,92 +1,74 @@
-// It allows to have mutliple immutable reference that is ok for the
-// borrow checker but we can modify them (so in fact we have multiple
-// mutable reference).
-use std::cell::RefCell;
-// Allow multiple ownership using reference counter
-// Weak pointers allows to avoid reference cycle (ie if two Rc point
-// to each other because in this case they cannot be dropped).
-use std::rc::{Rc, Weak};
+use std::cell::RefCell; // Used to mutate Rc
+use std::rc::Rc; // Reference Counted
 
-#[derive(Debug)]
-pub struct DbList<T> {
-    // todo
-    head: Option<Rc<RefCell<DbNode<T>>>>,
-    // if there is one element head and tail will point to the same location
-    // so we need to use a weak reference for one of them to be able to drop
-    // the element.
-    tail: Option<Weak<RefCell<DbNode<T>>>>,
+type Pointer<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    data: T,
+    next: Pointer<T>,
+    prev: Pointer<T>,
 }
 
-#[derive(Debug)]
-pub struct DbNode<T> {
-    data: T,
-    next: Option<Rc<RefCell<DbNode<T>>>>,
-    // Same than head & tail, we need to use a weak reference for one of the
-    // two.
-    prev: Option<Weak<RefCell<DbNode<T>>>>,
+impl<T: std::fmt::Display> Node<T> {
+    fn stringify(&self) -> String {
+        match &self.next {
+            None => format!("{}", self.data),
+            Some(next) => {
+                format!("{} -> {}", self.data, next.as_ref().borrow().stringify())
+            }
+        }
+    }
+}
+
+pub struct DbList<T> {
+    head: Pointer<T>,
+    tail: Pointer<T>,
 }
 
 impl<T> Default for DbList<T> {
     fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> DbList<T> {
+    pub fn new() -> Self {
         Self {
             head: None,
             tail: None,
         }
     }
-}
 
-impl<T> DbList<T> {
-    pub fn push_front(&mut self, data: T) {
+    pub fn push_head(&mut self, data: T) {
         match self.head.take() {
             None => {
-                let new_node = Rc::new(RefCell::new(DbNode {
+                let new_node = Rc::new(RefCell::new(Node {
                     data,
                     next: None,
                     prev: None,
                 }));
-                self.tail = Some(Rc::downgrade(&new_node));
+                self.tail = Some(Rc::clone(&new_node));
                 self.head = Some(new_node);
             }
             Some(old_head) => {
-                let new_node = Rc::new(RefCell::new(DbNode {
+                let new_node = Rc::new(RefCell::new(Node {
                     data,
-                    next: Some(old_head.clone()),
+                    next: Some(Rc::clone(&old_head)),
                     prev: None,
                 }));
-                // Now we need to modify the former head.
-                // So add new_node as a previous weak reference.
-                let mut m = old_head.borrow_mut();
-                m.prev = Some(Rc::downgrade(&new_node));
-                // and we update the head to point to the new node
+
+                old_head.borrow_mut().prev = Some(Rc::clone(&new_node));
                 self.head = Some(new_node);
             }
         }
     }
+}
 
-    pub fn push_back(&mut self, data: T) {
-        match self.tail.take() {
-            None => {
-                // It is the same as push front
-                let new_node = Rc::new(RefCell::new(DbNode {
-                    data,
-                    next: None,
-                    prev: None,
-                }));
-                self.tail = Some(Rc::downgrade(&new_node));
-                self.head = Some(new_node);
-            }
-            Some(old_tail) => {
-                let new_node = Rc::new(RefCell::new(DbNode {
-                    data,
-                    next: None,
-                    prev: Some(old_tail.clone()),
-                }));
-
-                let st = Weak::upgrade(&old_tail).unwrap();
-                let mut m = st.borrow_mut();
-                self.tail = Some(Rc::downgrade(&new_node));
-                m.next = Some(new_node);
-            }
+impl<T: std::fmt::Display> DbList<T> {
+    pub fn stringify(&self) -> String {
+        match &self.head {
+            None => String::from("List is empty"),
+            Some(node) => format!("HEAD: {}", node.as_ref().borrow().stringify()),
         }
     }
 }
